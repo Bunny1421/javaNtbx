4e25b73e7440823eaa9aa272d80fd6afa7920292
jirapa.khe <jirapa.khe@ntb.co.th>
2021-07-24 13:14:57 +0700

    Dip

--- hello/src/main/java/dip/Dip.java
+++ hello/src/main/java/dip/Dip.java
@@ -2,9 +2,13 @@ package dip;
 import java.util.Random;
 
 public class Dip {
+    private Random random;
+
     public String getId() {
-        Random random = new Random();
         int number = random.nextInt(10);
         return  "XYZ" + number;
+    }
+    public void setRandom(Random random) {
+        this.random = random;
     }
 }
--- hello/src/test/java/DipTest.java
+++ hello/src/test/java/DipTest.java
@@ -1,13 +1,34 @@
 import dip.Dip;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+
+
+class RandomX extends Random {
+    long number;
+    public RandomX(long seed) {
+        number = seed;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return (int) number;
+    }
+}
 public class DipTest {
+
+
     @Test
     @DisplayName("ต้องได้ id=XYZ7")
     public void case01() {
         Dip service = new Dip();
+        service.setRandom(new RandomX(7)); // Test Double = Stub
         String id = service.getId();
         assertEquals("XYZ7", id);
     }
